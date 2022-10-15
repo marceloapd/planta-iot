8dfa7c66264bf717b57602ef8bba1099903897bb
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-10-15 16:06:12 -0300

    corrige leitura do sensor

--- firmware/lib/sensorUmidadeSolo/sensorUmidadeSolo.cpp
+++ firmware/lib/sensorUmidadeSolo/sensorUmidadeSolo.cpp
@@ -2,17 +2,28 @@
 
 float getPercentHumidity() {
   int valorADC;
-  float umidadePercentual;
+  int value;
+  float umidadePercentual;'
 
   valorADC = analogRead(0); //1024 -> 3,3V
-  umidadePercentual = 100 * ((1024 - (float)valorADC) / 674);
-  umidadePercentual = 100 * ((980 - (float)valorADC) / 710);
+  Serial.println(valorADC);
+  // umidadePercentual = 100 * ((1024 - (float)valorADC) / 674);
+  // umidadePercentual = 100 * ((980 - (float)valorADC) / 710);  
+  // int max = 300;
+  // int min = 450;
+  // int range = max - min;
+  
+  // umidadePercentual = ((float)valorADC - min) * 100 / range;'
 
-  if (valorADC <= 270) {
-    umidadePercentual = 100.0;
-  } else if (valorADC >=1000) {
-    umidadePercentual = 0;
-  }
+  value = constrain(valorADC,560, 1024);
+  umidadePercentual = map(value,560,1024,100,0);
+
+
+  // if (valorADC <= 270) {
+  //   umidadePercentual = 100.0;
+  // } else if (valorADC >=1000) {
+  //   umidadePercentual = 0;
+  // }
 
   return umidadePercentual;
 }
