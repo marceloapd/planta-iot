41e9d0b3dc407bf5422de1d03d32624a3fedb08b
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-10-16 03:20:48 -0300

    feature de POST

--- firmware/lib/webServerAPI/webServerAPI.cpp
+++ firmware/lib/webServerAPI/webServerAPI.cpp
@@ -2,12 +2,15 @@
 #include <ESP8266WiFi.h>
 #include "sensorUmidadeSolo.h"
 #include "managerWifi.h"
-
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
 int port = 5000;
 ESP8266WebServer server(port);
+WiFiClient wifiClient;
+HTTPClient http; 
 
-void setHeaders () {
+void setHeaders() {
     server.sendHeader("Access-Control-Allow-Origin", "*");
     server.sendHeader("Access-Control-Allow-Headers", "*");
     server.sendHeader("Access-Control-Max-Age", "600");
@@ -43,6 +46,23 @@ void webServerInit() {
     String ipLocal = WiFi.localIP().toString();
     String ipMac = WiFi.macAddress();
     Serial.println("HTTP server started at " + ipLocal + ":" + String(port) + "\n\nMAC: " + ipMac );
+}
+
+int postReq(String url, String data) {
+    http.begin(wifiClient, url);     
+    http.addHeader("Content-Type", "application/json");  
+    int httpCode = http.POST(data);
+    http.end(); 
+    blinkLed();
+    return httpCode;
+}
+
+void sendData(){
+    Serial.println("sending data to API");
+    String data = getParams();
+    int statusCode = postReq("http://ptsv2.com/t/g5483-1665875753/post", data);
+    Serial.print("status code: ");
+    Serial.println(statusCode);
 }
 
 void handleClient () {
